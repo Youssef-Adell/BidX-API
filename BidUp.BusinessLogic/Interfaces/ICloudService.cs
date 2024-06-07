21d5481ec0acf29e979afa49d4496bcebf47b630
Youssef-Adell <al3nkpout2@gmail.com>
2024-06-07 07:30:52 +0300

    Add a method in cloud service to upload a thumbnail

--- BidUp.BusinessLogic/Interfaces/ICloudService.cs
+++ BidUp.BusinessLogic/Interfaces/ICloudService.cs
@@ -6,4 +6,9 @@ namespace BidUp.BusinessLogic.Interfaces;
 public interface ICloudService
 {
     Task<AppResult<UploadResponse>> UploadSvgIcon(Stream icon);
+
+    /// <summary>
+    /// Upload and transform the image to a thumbnail.
+    /// </summary>
+    Task<AppResult<UploadResponse>> UploadThumbnail(Stream image);
 }
