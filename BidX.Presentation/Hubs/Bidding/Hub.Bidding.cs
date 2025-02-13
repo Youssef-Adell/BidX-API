d8e63392acdfb7ac94aa5682ae058d94abcaf6ea
Youssef Adel <106375781+Youssef-Adell@users.noreply.github.com>
2025-02-13 14:31:01 +0200

    refactor:♻️group related SignalR methods together (#75)

--- BidX.Presentation/Hubs/Bidding/Hub.Bidding.cs
+++ BidX.Presentation/Hubs/Bidding/Hub.Bidding.cs
@@ -39,23 +39,4 @@ public partial class Hub
         var groupName = $"AUCTION#{request.AuctionId}";
         await Groups.RemoveFromGroupAsync(Context.ConnectionId, groupName);
     }
-
-
-    /// <summary>
-    /// The client must call this method when the auctions feed page loads to be able to receive feed updates in realtime
-    /// </summary>
-    public async Task JoinFeedRoom()
-    {
-        var groupName = "FEED";
-        await Groups.AddToGroupAsync(Context.ConnectionId, groupName);
-    }
-
-    /// <summary>
-    /// The client must call this method when the auctions feed page is about to be closed to stop receiving unnecessary feed updates
-    /// </summary>
-    public async Task LeaveFeedRoom()
-    {
-        var groupName = "FEED";
-        await Groups.RemoveFromGroupAsync(Context.ConnectionId, groupName);
-    }
 }
