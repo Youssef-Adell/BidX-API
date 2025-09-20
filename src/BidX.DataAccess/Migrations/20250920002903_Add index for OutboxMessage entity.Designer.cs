308498531a73f25fdef800635f72a114782f5f8a
Youssef Adel <al3nkpout2@gmail.com>
2025-09-20 03:25:14 +0300

    Add Index for OutboxMessage entity

--- src/BidX.DataAccess/Migrations/20250920002903_Add index for OutboxMessage entity.Designer.cs
+++ src/BidX.DataAccess/Migrations/20250920002903_Add index for OutboxMessage entity.Designer.cs
@@ -4,6 +4,7 @@ using BidX.DataAccess;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BidX.DataAccess.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250920002903_Add index for OutboxMessage entity")]
+    partial class AddindexforOutboxMessageentity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
